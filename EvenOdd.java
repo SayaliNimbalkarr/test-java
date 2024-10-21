92031dfa18325e120ef83cfe9261f9eed7ab54f3
sayalii <123319262+SayaliNimbalkarr@users.noreply.github.com>
2024-10-21 18:20:42 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        // Option 1: Use predefined input
+        int number = 10; // Replace 10 with the number you want to test
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
